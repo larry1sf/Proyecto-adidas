2998d8b4eb6f36dbb7d238e649a1d17874e0f3a6
larry1sf <93235154+larry1sf@users.noreply.github.com>
2024-11-18 07:28:49 -0500

    index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ["./src/*.html", "./src/**/*.html"];
 export const theme = {
+
   color: {
-    "primary": "#111"
   },
 };
 export const plugins = [];
